078d84b5bf166733c8abea74b928e01e1a2085eb
Kyros Echaporia <124595523+KyrosEchaporia@users.noreply.github.com>
2023-10-08 10:46:19 +0530

    Add files via upload

---
+++ setoperation.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct  8 10:42:32 2023
+
+@author: Kyros Achaporia
+"""
+
+A = {2, 4, 6, 8, 10};
+B = {1, 2, 3, 4, 5};
+
+print("\n Union of A and B is",A | B)
+
+print("\n Intersection of A and B is",A & B)
+
+print("\n Difference of A and B is",A - B)
+
+print("\n Symmetric difference of A and B is",A ^ B)
