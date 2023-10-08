a55af4b65906ed617fa285efa3439ddcb5b4b5ad
Kyros Echaporia <124595523+KyrosEchaporia@users.noreply.github.com>
2023-10-08 10:39:39 +0530

    Add files via upload

---
+++ positiveno.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct  8 10:37:14 2023
+
+@author: Kyros Echaporia
+"""
+
+list1 = [13, -21, 0, 47, 65, -99]
+print("Positive numbers in",list1,"are")
+for num in list1:
+       if num >= 0:
+           print(num, end = " ")
